3b52dc53c95210d5fda9f0262ddf339e32d4126f
Colin Cummings <colin_cummings@brown.edu>
2019-10-24 08:16:38 -0400

    test selection after clear

--- src/App.test.js
+++ src/App.test.js
@@ -6,8 +6,15 @@ import App from "./App";
 it("clears single selection", async () => {
   const { getByLabelText, getByText } = render(<App />);
   const select = getByLabelText("Selection");
+  // no selection
+  getByText("Select an option.");
+  // select option 1
   await selectEvent.select(select, "Option 1");
   getByText("You selected option 1.");
+  // clear selection
   selectEvent.clearFirst(select);
-  await wait(() => expect(getByText("Select an option.")).toBeInTheDocument()); // fails
+  await wait(() => expect(getByText("Select an option.")).toBeInTheDocument());
+  // select option 2
+  await selectEvent.select(select, "Option 2"); // fails
+  getByText("You selected option 2.");
 });
